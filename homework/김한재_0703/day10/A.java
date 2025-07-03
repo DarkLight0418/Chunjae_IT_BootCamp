ba8c1115f6a3217a16d50bec415701c1973e0f77
DarkLight0418 <samseong0418@naver.com>
2025-07-04 08:35:55 +0900

    20250703 class

---
+++ homework/김한재_0703/day10/A.java
@@ -0,0 +1,19 @@
+package day10;
+
+
+public class A {
+    public int i;
+    
+    protected  void m() {
+        System.out.println("m()");
+
+    }
+}
+
+class A1 {
+    public int i;
+    
+    protected  void m() {
+        System.out.println("m()");
+    }
+}
---
+++ homework/김한재_0703/day10/F.java
@@ -0,0 +1,24 @@
+package day10;
+
+public class F {
+    String name;
+    F() {
+    }
+    public static void main(String[] args) {
+        F f = new F();
+        System.out.println("");
+    }
+}
+
+class Helper {
+    F f;
+    Helper(F f) {
+        this.f = f;
+    }
+
+    void work() {
+        //f.m();
+    }
+}
+
+
