151163baac2dbd1189950e233625a6e0c9fa3d96
DarkLightBegin23 <samseong0418@naver.com>
2025-07-05 11:16:14 +0900

    20250705 java jdk version...

--- homework/김한재_0704/day11_20250704/src/main/java/day11/A1.java
+++ homework/김한재_0704/day11_20250704/src/main/java/day11/A1.java
@@ -11,3 +11,15 @@ public abstract interface A1 {
         System.out.println("m3()");
     }
 }
+
+public abstract interface A1_1 {
+    public static final int I = 2;
+    public abstract void m1();
+    
+    public default void m2() {
+        System.out.println("m2()");
+    }
+    public static void m3() {
+        System.out.println("m3()");
+    }
+}
