dc6f221a99e21e6b371589f00bcaa1eb0eb94eaa
DarkLight0418 <samseong0418@naver.com>
2025-07-07 17:33:41 +0900

    20250707 class

---
+++ class/week04/Day12/src/D.java
@@ -0,0 +1,37 @@
+package day12;
+
+import soo.P;
+
+class DTh extends Thread {
+    public void run() {
+        D.m();
+    }
+}
+
+public class D {
+    static int i;  // 무결성을 깨버림
+    static int k;
+    D() {
+        Thread th1 = new DTh();
+        th1.start();  // JVM -> new 제어 -> run()
+
+        Thread th2 = new DTh();
+        th2.start();  //
+        try {
+            th1.join();
+            th2.join();  // 계속 함께 하는 거래요
+        } catch (InterruptedException ie) {}
+        
+        P.pln("i: " + i);  // main()
+    }
+
+    synchronized static void m() {  // synchronized
+        for(int j=0; j<10000; j++) {
+            k++;
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        new D();
+    }
+}
