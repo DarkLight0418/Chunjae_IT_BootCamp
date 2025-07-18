9c54f284436b598c311c75cd4fad84f7cc26e879
DarkLightBegin23 <samseong0418@naver.com>
2025-07-19 08:43:39 +0900

    merged 20250719

---
+++ IntelliJ_project/i-pj/src/main/java/day09_OOP_Exception/G.java
@@ -0,0 +1,30 @@
+﻿package day09_OOP_Exception;
+
+import soo.P;
+
+public class G {
+    public static void main(String[] args) {
+        boolean flag = true;
+        try {
+            P.pln("(1)");
+            ExcepUser ex = new ExcepUser();
+            P.pln("(2)");
+            if(flag) System.exit(-1); //0:정상종료, -1:비정상종료
+            ex.m();
+            P.pln("(3)");
+            //ex.mm();
+            P.pln("(4)");
+        }catch(MyException me) {
+            P.pln("(5)");
+        }catch(YourException ye) {
+            P.pln("(6)");
+        }catch(Exception e) {
+            if(flag) return;
+            P.pln("(7)");
+        }finally {
+            P.pln("(8)");
+        }
+
+        P.pln("(9)");
+    }
+}
