f47265dc933c629849d11388eab96c5c3251aa2c
DarkLightBegin23 <samseong0418@naver.com>
2025-07-24 21:49:19 +0900

    20250724 self-study

--- JDBC_GUI/JDBC_GUI_Project/src/main/java/gui/day01_self/A3.java
+++ JDBC_GUI/JDBC_GUI_Project/src/main/java/gui/day01_self/A3.java
@@ -29,3 +29,28 @@ public class A3 extends JFrame {
         a3.init();
     }
 }
+
+class A3_2 extends JFrame {
+    JButton b;
+    void init() {
+        b = new JButton("자바의 버튼");
+        ActionListener handler = new A3_2Handler(this);
+        b.addActionListener(handler);
+        add(b);
+        setUI();
+    }
+
+    void setUI() {
+        setTitle("GUI Test Ver 1.0");
+        setVisible(true);
+        setSize(300, 200);
+        setLocation(200, 100);
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        A3_2 a3 = new A3_2();
+        a3.init();
+    }
+}
--- JDBC_GUI/JDBC_GUI_Project/src/main/java/jdbc/day01_assignment/B.java
+++ JDBC_GUI/JDBC_GUI_Project/src/main/java/jdbc/day01_assignment/B.java
@@ -1,24 +1,74 @@
 package jdbc.day01_assignment;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 class B {
     String maria = "org.mariadb.jdbc.Driver";
     String mariaURL = "jdbc:mariadb://127.0.0.1:3306/java_schema";
 
     Connection con;
+    Statement stmt;
 
     B() {
         try {
             Class.forName(maria);
             con = DriverManager.getConnection(mariaURL, "scott", "tiger");
-            System.out.println("드라이버 로딩 성공");
+            stmt = con.createStatement();
+
         } catch (ClassNotFoundException ce) {
             System.out.println("실패 :" + ce);
         } catch (SQLException se) {
             System.out.println("연결 실패 :" + se);
+        }
+        createT();
+        closeAll();
+    }
+    String tname = "JDBCT";
+    void createT() {
+        String sql = "create table if not exists " + tname + "(NO int primary key, NAME varchar(10), RDATE datetime)";
+
+        try {
+            System.out.println("테이블 생성 성공");
+            stmt.execute(sql);
+        } catch (SQLException se) {
+            System.out.println("연결 실패");
+        }
+    }
+    void dropT() {
+        String sql = "drop table if exists " + tname;
+        try {
+            stmt.execute(sql);
+            System.out.println("테이블 삭제 성공");
+        } catch (SQLException se) {
+            System.out.println("테이블 삭제 실패 : " + se);
+        }
+    }
+
+    void insertD(int no, String name) {
+        String sql = "insert into " + tname + " values(" + no +",'"+name+"', now())";
+        try {
+            int i = stmt.executeUpdate(sql);
+            if(i>0) {
+                System.out.println(i + "개의 row 입력 성공");
+            } else {
+                System.out.println("입력 실패");
+            }
+        } catch (SQLException se) {
+            System.out.println("입력 실패: " + se);
+        }
+    }
+
+    void selectD() {
+        String sql = "select * from " + tname + " order by NO desc";
+        ResultSet rs = null;
+    }
+
+    void closeAll() {
+        try {
+            stmt.close();
+            con.close();
+            System.out.println("객체들 닫기 성공");
+        } catch (SQLException se) {
         }
     }
 
