6f7f38aedd9f24bb24459768abe65c3b11a7d5a6
DarkLightBegin23 <samseong0418@naver.com>
2025-08-30 18:46:54 +0900

    20250830 프로젝트 일부 백업

--- WebServer/workspace/got-it-temp/src/main/java/com/gotit/board/PostDAO.java
+++ WebServer/workspace/got-it-temp/src/main/java/com/gotit/board/PostDAO.java
@@ -1,11 +1,23 @@
-package gotit.board;
+package com.gotit.board;
 
 import java.sql.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class PostDAO {
 	private DataSource ds;
 	PostDAO() {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("");
+		} catch (NamingException ne) {
+			System.out.println("네이밍 예외 발생 : " + ne);
+		}
+	}
+	
+	public void close() {
 	}
 }
