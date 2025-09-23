288d56d074d2d71285d25a110fb2fa7368c18f52
DarkLightBegin23 <samseong0418@naver.com>
2025-09-23 22:13:38 +0900

    20250923 집 와서 조금 작업함

--- Spring_boot/boot/src/test/java/khj/app/boot/service/BookServiceTest.java
+++ Spring_boot/boot/src/test/java/khj/app/boot/service/BookServiceTest.java
@@ -1,9 +1,13 @@
 package khj.app.boot.service;
 
+import khj.app.boot.domain.Author;
 import khj.app.boot.domain.Book;
+import khj.app.boot.repository.SpringDataJpaMariaAuthorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,14 +17,29 @@ class BookServiceTest {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private SpringDataJpaMariaAuthorRepository authorRepo;
+
 
     @Test
     void insertBook() {
+        Author author = new Author();
+        author.setName("갓창우");
+        authorRepo.save(author);
+
         Book book = new Book();
+        book.setTitle("프로그래밍 천재의 삶");
+        book.setAuthor(author);
+        bookService.insertBook(book);
+        System.out.println("insertBook() 메소드 잘 들어갔니~?" + book);
     }
 
     @Test
     void getBooks() {
+        List<Book> books = bookService.getBooks();
+        for (Book book : books) {
+            System.out.println("book: " + book);
+        }
     }
 
     @Test
